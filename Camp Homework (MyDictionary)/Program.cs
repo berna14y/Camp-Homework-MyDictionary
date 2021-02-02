19fe29a0f4ef50422a92d73af758c171fffbd92a
Berna Yılmaz <berna14y@gmail.com>
2021-02-03 00:25:17 +0300

    Add project files.

---
+++ Camp Homework (MyDictionary)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<string, string> mydict = new MyDict<string, string>();
+            mydict.Add("UK", "London, Manchester, Birmingham");
+            mydict.Add("USA", "Chicago, New York, Washington");
+            mydict.Add("India", "Mumbai, New Delhi, Pune");
+        }
+    }
+}
